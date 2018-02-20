744a4f783e7286866101b4d45e030b813471e79a
LeeMyeongHoon <xlatmvkt@gmail.com>
2018-02-20 21:09:43 +0900

    ss

---
+++ 가장 긴 바이토닉 부분 수열/sol2.cpp
@@ -0,0 +1,38 @@
+//// https://www.acmicpc.net/problem/11054
+//
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//#include <string>
+//
+//struct Soluter
+//{
+//	int count;
+//	std::vector<int> arr;
+//
+//	void Input()
+//	{
+//		std::cin >> count;
+//		arr.assign(count, {});
+//		for (int i = 0; i < count; i++)
+//		{
+//			std::cin >> arr[i];
+//		}
+//	}
+//
+//	void Solve()
+//	{
+//	}
+//};
+//
+//int main()
+//{
+//	std::ios::sync_with_stdio(0);
+//	std::cin.tie(0);
+//
+//	Soluter soluter;
+//	soluter.Input();
+//	soluter.Solve();
+//
+//	return 0;
+//}
